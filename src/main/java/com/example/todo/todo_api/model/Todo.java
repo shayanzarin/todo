0a36988ef455165ef00981593872a5f6503e6ee7
shayanzarin <shayan.zarin.pars@gmail.com>
2025-04-17 00:06:28 +0200

    model

--- src/main/java/com/example/todo/todo_api/model/Todo.java
+++ src/main/java/com/example/todo/todo_api/model/Todo.java
@@ -8,6 +8,51 @@ public class Todo {
 	private String descripton;
 	private boolean completed;
 	
+	public Todo() {
+		this.id=UUID.randomUUID();
+	}
+
+	public Todo(String title, String descripton) {
+		this();
+		this.title = title;
+		this.descripton = descripton;
+		this.completed = false;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescripton() {
+		return descripton;
+	}
+
+	public void setDescripton(String descripton) {
+		this.descripton = descripton;
+	}
+
+	public boolean isCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+	
+	
+	
 	
 
 }
