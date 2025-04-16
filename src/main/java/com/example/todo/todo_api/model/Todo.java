9d36218e1683ef27e2155491e8db6c2dc2f0787f
shayanzarin <shayan.zarin.pars@gmail.com>
2025-04-17 00:05:35 +0200

    model

---
+++ src/main/java/com/example/todo/todo_api/model/Todo.java
@@ -0,0 +1,13 @@
+package com.example.todo.todo_api.model;
+
+import java.util.UUID;
+
+public class Todo {
+	private UUID id;
+	private String title;
+	private String descripton;
+	private boolean completed;
+	
+	
+
+}
