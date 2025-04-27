443ced979513d957c050716017a42e3f4f0fba0e
shayanzarin <shayan.zarin.pars@gmail.com>
2025-04-27 13:25:33 +0200

    interface service

--- src/main/java/com/example/todo/todo_api/service/TodoService.java
+++ src/main/java/com/example/todo/todo_api/service/TodoService.java
@@ -1,5 +1,14 @@
 package com.example.todo.todo_api.service;
 
-public interface TodoService {
+import java.util.List;
+import java.util.UUID;
 
+import com.example.todo.todo_api.model.Todo;
+
+public interface TodoService {
+	List<Todo> getAllTodos();
+	Todo getTodoByID(UUID id);
+	Todo creatTodo(Todo todo);
+	Todo updateTodo(UUID id, Todo updatedTodo);
+	void deleteTodo(UUID id);
 }
