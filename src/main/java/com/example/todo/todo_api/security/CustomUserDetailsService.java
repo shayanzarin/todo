e87184585519abaaef245d499bc7c03de85f77c5
shayanzarin <shayan.zarin.pars@gmail.com>
2025-05-04 17:08:51 +0200

    spring security and config

---
+++ src/main/java/com/example/todo/todo_api/security/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.example.todo.todo_api.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import java.util.stream.Collectors;
+
+import com.example.todo.todo_api.repository.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService{
+	
+	private final UserRepository userRepository;
+	
+	@Autowired
+	public CustomUserDetailsService (UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	@Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.example.todo.todo_api.model.User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList())
+        );
+    }
+	
+	
+	
+
+
+}
