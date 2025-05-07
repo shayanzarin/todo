5a3be144f70427a7d990dda1bdfb5bcc19d8731d
shayanzarin <shayan.zarin.pars@gmail.com>
2025-05-07 22:01:42 +0200

    config

--- src/main/java/com/example/todo/todo_api/config/SecurityConfig.java
+++ src/main/java/com/example/todo/todo_api/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.example.todo.todo_api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,20 +9,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.example.todo.todo_api.security.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	private final CustomUserDetailsService userDetailsService;
+
+    private final CustomUserDetailsService userDetailsService;
 
     @Autowired
     public SecurityConfig(CustomUserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
     }
-    
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -40,22 +40,17 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/register").permitAll()
-                        .requestMatchers("/css/**", "/js/**").permitAll()
                         .requestMatchers("/todos/new", "/todos/save").authenticated()
                         .requestMatchers("/todos/edit/**", "/todos/update/**", "/todos/delete/**").authenticated()
-                        .requestMatchers("/todos").authenticated()
-                        .anyRequest().permitAll()
+                        .anyRequest().permitAll() 
                 )
                 .formLogin((form) -> form
-                        .loginPage("/login")
+                        .loginPage("/login") 
                         .permitAll()
+                        .failureUrl("/login?error")
+                        .defaultSuccessUrl("/todos", true)
                 )
-                .logout((logout) -> logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .permitAll()
-                )
-                .authenticationProvider(authenticationProvider());
+                .logout((logout) -> logout.permitAll());
 
         return http.build();
     }
